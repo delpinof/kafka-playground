ffe24f005a7d0fedbdc0fc2b57c0902105bc8e20
Fernando del Pino <fherdelpino@hotmail.com>
2023-07-22 02:22:15 +1000

    create multimodule skeleton

---
+++ kafka-consumer/src/test/java/com/fherdelpino/playground/consumer/KafkaPlaygroundConsumerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fherdelpino.playground.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KafkaPlaygroundConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ kafka-producer/src/main/java/com/fherdelpino/kafka/playground/producer/KafkaPlaygroundProducerApplication.java
@@ -0,0 +1,12 @@
+package com.fherdelpino.kafka.playground.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPlaygroundProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPlaygroundProducerApplication.class, args);
+    }
+}
