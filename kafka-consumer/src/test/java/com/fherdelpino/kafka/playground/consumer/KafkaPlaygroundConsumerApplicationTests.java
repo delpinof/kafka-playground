6a99f6b2cb828a33eba6e54d3a896e83ce3a0356
Fernando del Pino <fherdelpino@hotmail.com>
2023-07-22 15:30:06 +1000

    fix consumer package

--- kafka-consumer/src/test/java/com/fherdelpino/kafka/playground/consumer/KafkaPlaygroundConsumerApplicationTests.java
+++ kafka-consumer/src/test/java/com/fherdelpino/kafka/playground/consumer/KafkaPlaygroundConsumerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fherdelpino.playground.consumer;
+package com.fherdelpino.kafka.playground.consumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
