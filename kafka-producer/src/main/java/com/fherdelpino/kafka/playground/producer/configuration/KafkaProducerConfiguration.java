feb22b0d50a55575bd7bc203939a56796581a3ed
Fernando del Pino <fherdelpino@hotmail.com>
2023-07-26 01:14:41 +1000

    add streams

--- kafka-producer/src/main/java/com/fherdelpino/kafka/playground/producer/configuration/KafkaProducerConfiguration.java
+++ kafka-producer/src/main/java/com/fherdelpino/kafka/playground/producer/configuration/KafkaProducerConfiguration.java
@@ -15,8 +15,8 @@ import java.util.Properties;
 @Configuration
 public class KafkaProducerConfiguration {
 
-    @Value("${kafka.bootstrap-server}")
-    private String bootstrapServer;
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Value("${kafka.schema-registry}")
     private String schemaRegistry;
@@ -24,7 +24,7 @@ public class KafkaProducerConfiguration {
     @Bean
     public Producer<String, String> kafkaStringProducer() {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return new KafkaProducer<>(props);
@@ -33,7 +33,7 @@ public class KafkaProducerConfiguration {
     @Bean
     public Producer<String, Student> kafkaAvroProducer() {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
         props.put("schema.registry.url", schemaRegistry);
---
+++ kafka-streams/src/main/java/com/fherdelpino/kafka/playground/streams/service/KafkaPlaygroundStreamCommandLineRunner.java
@@ -0,0 +1,48 @@
+package com.fherdelpino.kafka.playground.streams.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Produced;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Slf4j
+@Component
+public class KafkaPlaygroundStreamCommandLineRunner implements CommandLineRunner {
+
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${kafka.input-topic}")
+    private String inputTopic;
+
+    @Value("${kafka.output-topic}")
+    private String outputTopic;
+
+    @Value("${kafka.application-id}")
+    private String applicationId;
+    @Override
+    public void run(String... args) {
+        Properties streamsProps = new Properties();
+        streamsProps.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        streamsProps.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId);
+        StreamsBuilder builder = new StreamsBuilder();
+
+        builder.stream(inputTopic, Consumed.with(Serdes.String(), Serdes.String()))
+                .mapValues(value -> value + " is streamed")
+                //.peek((key, value) -> log.info("key: {} - value: {}", key, value))
+                .to(outputTopic, Produced.with(Serdes.String(), Serdes.String()));
+
+        try(KafkaStreams kafkaStreams = new KafkaStreams(builder.build(), streamsProps)) {
+            kafkaStreams.start();
+        }
+
+    }
+}
