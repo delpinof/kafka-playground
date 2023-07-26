e51f08289b0484b05f0663e1622a8cb2aba8dd35
Fernando del Pino <fherdelpino@hotmail.com>
2023-07-26 21:05:04 +1000

    add conditional on property for different types of streams

--- kafka-streams/src/main/java/com/fherdelpino/kafka/playground/streams/service/KafkaPlaygroundAvroStreamsCommandLineRunner.java
+++ kafka-streams/src/main/java/com/fherdelpino/kafka/playground/streams/service/KafkaPlaygroundAvroStreamsCommandLineRunner.java
@@ -1,10 +1,56 @@
 package com.fherdelpino.kafka.playground.streams.service;
 
+import com.fherdelpino.kafka.playground.common.avro.model.Student;
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
 
+import java.util.Properties;
+
+@Slf4j
+@Component
+@ConditionalOnProperty(prefix = "playground", name = "stream-type", havingValue = "avro")
 public class KafkaPlaygroundAvroStreamsCommandLineRunner implements CommandLineRunner {
-    @Override
-    public void run(String... args) throws Exception {
 
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${kafka.input-topic}")
+    private String inputTopic;
+
+    @Value("${kafka.output-topic}")
+    private String outputTopic;
+
+    @Value("${kafka.schema-registry}")
+    private String schemaRegistry;
+
+    @Value("${kafka.application-id}")
+    private String applicationId;
+
+    @Override
+    public void run(String... args) {
+        Properties streamsProps = new Properties();
+        streamsProps.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        streamsProps.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId);
+        streamsProps.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, GenericAvroSerde.class);
+        streamsProps.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+        streamsProps.put("schema.registry.url", schemaRegistry);
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, Student> stream = builder.stream(inputTopic);
+        stream.mapValues(student -> Student.newBuilder(student).setStudentName("Alonso").build())
+                //.peek((key, value) -> log.info("key: {} - value: {}", key, value))
+                .to(outputTopic);
+
+        KafkaStreams kafkaStreams = new KafkaStreams(builder.build(), streamsProps);
+        kafkaStreams.start();
     }
 }
--- kafka-streams/src/main/java/com/fherdelpino/kafka/playground/streams/service/KafkaPlaygroundStringStreamsCommandLineRunner.java
+++ kafka-streams/src/main/java/com/fherdelpino/kafka/playground/streams/service/KafkaPlaygroundStringStreamsCommandLineRunner.java
@@ -1,21 +1,22 @@
 package com.fherdelpino.kafka.playground.streams.service;
 
-import com.fherdelpino.kafka.playground.common.avro.model.Student;
-import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Produced;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.util.Properties;
 
 @Slf4j
 @Component
+@ConditionalOnProperty(prefix = "playground", name = "stream-type", havingValue = "string")
 public class KafkaPlaygroundStringStreamsCommandLineRunner implements CommandLineRunner {
 
     @Value("${kafka.bootstrap-servers}")
@@ -27,9 +28,6 @@ public class KafkaPlaygroundStringStreamsCommandLineRunner implements CommandLin
     @Value("${kafka.output-topic}")
     private String outputTopic;
 
-    @Value("${kafka.schema-registry}")
-    private String schemaRegistry;
-
     @Value("${kafka.application-id}")
     private String applicationId;
 
@@ -38,17 +36,16 @@ public class KafkaPlaygroundStringStreamsCommandLineRunner implements CommandLin
         Properties streamsProps = new Properties();
         streamsProps.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         streamsProps.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId);
-        streamsProps.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, GenericAvroSerde.class);
-        streamsProps.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
-        streamsProps.put("schema.registry.url", schemaRegistry);
         StreamsBuilder builder = new StreamsBuilder();
 
-        KStream<String, Student> stream = builder.stream(inputTopic);
-        stream.mapValues(student -> Student.newBuilder(student).setStudentName("Alonso").build())
+        builder.stream(inputTopic, Consumed.with(Serdes.String(), Serdes.String()))
+                .mapValues(value -> value + " is streamed")
                 //.peek((key, value) -> log.info("key: {} - value: {}", key, value))
-                .to(outputTopic);
+                .to(outputTopic, Produced.with(Serdes.String(), Serdes.String()));
 
-        KafkaStreams kafkaStreams = new KafkaStreams(builder.build(), streamsProps);
-        kafkaStreams.start();
+        try (KafkaStreams kafkaStreams = new KafkaStreams(builder.build(), streamsProps)) {
+            kafkaStreams.start();
+        }
+
     }
 }
