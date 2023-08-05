6216309f905e53d7e109d1e73e745d7438af61dd
Fernando del Pino <fherdelpino@hotmail.com>
2023-08-06 02:19:44 +1000

    add test configuration and teardown

--- kafka-streams/src/test/java/com/fherdelpino/kafka/playground/streams/service/runnner/KafkaPlaygroundStringStreamsCommandLineRunnerTest.java
+++ kafka-streams/src/test/java/com/fherdelpino/kafka/playground/streams/service/runnner/KafkaPlaygroundStringStreamsCommandLineRunnerTest.java
@@ -7,6 +7,7 @@ import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.TestInputTopic;
 import org.apache.kafka.streams.TestOutputTopic;
 import org.apache.kafka.streams.TopologyTestDriver;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -17,38 +18,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class KafkaPlaygroundStringStreamsCommandLineRunnerTest {
 
-
-    private static KafkaPlaygroundStringStreamsCommandLineRunner sut;
-
     private static final String INPUT_TOPIC_NAME = "string-topic-input";
-
     private static final String OUTPUT_TOPIC_NAME = "string-topic-output";
-
     private static final String SCHEMA_REGISTRY = "mock://aggregation-test";
 
-    private static final Properties STREAM_PROPERTIES = new Properties();
+    private static TopologyTestDriver testDriver;
+    private static TestInputTopic<String, String> inputTopic;
+    private static TestOutputTopic<String, String> outputTopic;
 
-    private final static StreamsBuilder builder = new StreamsBuilder();
 
     @BeforeAll
-    public static void init() {
-        STREAM_PROPERTIES.put(StreamsConfig.APPLICATION_ID_CONFIG, "string-test");
-        STREAM_PROPERTIES.put("schema.registry.url", SCHEMA_REGISTRY);
-        sut = new KafkaPlaygroundStringStreamsCommandLineRunner(STREAM_PROPERTIES, INPUT_TOPIC_NAME, OUTPUT_TOPIC_NAME);
+    public static void configure() {
+        Properties streamProps = new Properties();
+        streamProps.put(StreamsConfig.APPLICATION_ID_CONFIG, "string-test-app");
+        streamProps.put("schema.registry.url", SCHEMA_REGISTRY);
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KafkaPlaygroundStringStreamsCommandLineRunner sut = new KafkaPlaygroundStringStreamsCommandLineRunner(
+                streamProps, INPUT_TOPIC_NAME, OUTPUT_TOPIC_NAME);
+        sut.createBuilder(builder);
+
+        testDriver = new TopologyTestDriver(builder.build(), streamProps);
+        inputTopic = testDriver.createInputTopic(INPUT_TOPIC_NAME, Serdes.String().serializer(), Serdes.String().serializer());
+        outputTopic = testDriver.createOutputTopic(OUTPUT_TOPIC_NAME, Serdes.String().deserializer(), Serdes.String().deserializer());
     }
 
     @Test
     public void test() {
         List<String> expectedValues = List.of("1 is streamed", "2 is streamed", "3 is streamed");
-        sut.createBuilder(builder);
-        try (final TopologyTestDriver testDriver = new TopologyTestDriver(builder.build(), STREAM_PROPERTIES)) {
-            final TestInputTopic<String, String> inputTopic = testDriver.createInputTopic(INPUT_TOPIC_NAME, Serdes.String().serializer(), Serdes.String().serializer());
-            final TestOutputTopic<String, String> outputTopic = testDriver.createOutputTopic(OUTPUT_TOPIC_NAME, Serdes.String().deserializer(), Serdes.String().deserializer());
-            for (int i = 1; i <= 3; i++) {
-                inputTopic.pipeInput(String.valueOf(i), String.valueOf(i));
-            }
-            List<String> actualValues = outputTopic.readValuesToList();
-            assertEquals(expectedValues, actualValues);
+        for (int i = 1; i <= 3; i++) {
+            inputTopic.pipeInput(String.valueOf(i), String.valueOf(i));
         }
+        List<String> actualValues = outputTopic.readValuesToList();
+        assertEquals(expectedValues, actualValues);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        testDriver.close();
     }
 }
